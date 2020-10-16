0e1a820be411af27b077f3a5168480f3ee0f8492
Mike Antekov <antekov@yandex.ru>
2020-10-16 21:36:20 +0300

    Refactoring

--- cpp/Algo/taskA/taskA.cpp
+++ cpp/Algo/taskA/taskA.cpp
@@ -3,100 +3,80 @@
 #include <vector>
 
 class Solution {
-    std::string pattern, str, buffer;
-    std::vector<size_t> entries;
-    std::vector<size_t> z;
+    std::string pattern_, buffer_;
+    std::vector<size_t> entries_;
+    
     size_t left, right, pos;
-    size_t m; 
-    int count;
-    int buffer_size;
+    size_t symbol_count_;
+    size_t buffer_size_;
 
-    void add(char ch) {
-        //str += ch;
-        count++;
+    void addSymbol(char ch) {
+        symbol_count_++;
 
-        if (buffer.length() < buffer_size) {
-            buffer += ch;
+        if (buffer_.length() < buffer_size_) {
+            buffer_ += ch;
         } else {
-            buffer[count % buffer_size] = ch;
+            buffer_[symbol_count_ % buffer_size_] = ch;
         }
-
-        //std::cout << str << std::endl;
-        //std::cout << buffer << std::endl;
     }
 
-    char get(size_t shift) {
-        char ch;
-
-        size_t needed_pos = shift;
-
-        ch = str[needed_pos];
-        
-
-        return ch;
-    }
-
-    char getc(size_t shift) {
-        char ch;
-
-        if (shift < buffer_size) {
-            return buffer[shift];
+    char getSymbol(size_t index) {
+        if (index < buffer_size_) {
+            return buffer_[index];
         }
 
-        ch = buffer[(shift + 1) % buffer_size];
-        
-        return ch;
+        return buffer_[(index + 1) % buffer_size_];
     }
 
     bool isComplete() {
-        return (pos > count);
+        return (pos > symbol_count_);
     }
-
     
 public:
-    Solution(std::string &pattern) : 
-        pattern(pattern), 
-        str(pattern),
-        buffer(pattern),
-        m(pattern.length()), 
-        count(m),
-        buffer_size(m+1) {}
+    Solution(std::string &pattern_) : 
+        pattern_(pattern_),
+        buffer_(pattern_),
+        symbol_count_(pattern_.length()), 
+        buffer_size_(pattern_.length() + 1) {}
 
-    void solve() {
+    std::vector<size_t> solve() {
         char ch;
-        z.resize(m);
-        z[0] = 0;
-        pos = 1, left = 0, right = 0;
         size_t z_value = 0;
-        
+        size_t m = pattern_.length();
+        std::vector<size_t> z_function(m);
 
+        z_function[0] = 0;
+        pos = 1;
+        left = 0;
+        right = 0;
+        
         while (!isComplete()) {
-            //std::cout << "pos | left | right | count = " << pos << " | " << left << " | " << right << " | " << count << std::endl;
+            //std::cout << "pos | left | right | symbol_count_ = " << pos << " | " << left << " | " << right << " | " << symbol_count_ << std::endl;
             
             if (pos <= right) {
-                z_value = std::min(right - pos + 1, z[pos - left]);
+                z_value = std::min(right - pos + 1, z_function[pos - left]);
             } else {
                 z_value = 0;
             }
             while (true) {
                 if (pos + z_value < m) {
-                    //std::cout << "p[" << z_value << "]=" << pattern[z_value] << std::endl;
-                    if (pattern[z_value] == pattern[pos + z_value] && z_value < m) {
+                    //std::cout << "p[" << z_value << "]=" << pattern_[z_value] << std::endl;
+                    if (pattern_[z_value] == pattern_[pos + z_value] && z_value < m) {
                         z_value++;
                     } else {
                         break;
                     }
                 } else {
                     
-                    if (pos + z_value >= count - 1)
+                    if (pos + z_value >= symbol_count_ - 1)
                         if (std::cin >> ch) {
-                            add(ch);
+                            addSymbol(ch);
                         }
 
                     
                     //std::cout << " get[" << pos + z_value << "]=" << get(pos + z_value) << std::endl;
                     //std::cout << "getc[" << pos + z_value << "]=" << getc(pos + z_value) << std::endl;
-                    if (pos + z_value < count && pattern[z_value] == getc(pos + z_value) && z_value < m) {
+                    if (pos + z_value < symbol_count_ && pattern_[z_value] == getSymbol(pos + z_value) && z_value < m) {
                         z_value++;
                     } else {
                         break;
@@ -106,7 +86,7 @@ public:
             //std::cout << "z[" << pos << "]=" << z_value << std::endl;
             
             if (pos < m) {
-                z[pos] = z_value;
+                z_function[pos] = z_value;
             }
 
             if (pos + z_value - 1 > right) {
@@ -115,30 +95,24 @@ public:
             }
 
             if (pos >= m && z_value == m) {
-                entries.push_back(pos - m);
+                entries_.push_back(pos - m);
             }
         
             pos++;
         }
-
-    }
-
-    void print() {
-        for (auto i : entries) {
-            std::cout << i << ' ';
-        }
+        return entries_;
     }
 };
 
 
 
 int main() {
-    freopen("input.txt", "r", stdin);
     std::string pattern;
     std::cin >> pattern;
 
     Solution solution(pattern);
-    solution.solve();
-    
-    solution.print();
+
+    for (auto i : solution.solve()) {
+        std::cout << i << ' ';
+    }
 }
