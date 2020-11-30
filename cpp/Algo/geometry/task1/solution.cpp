c5687c76d8017b1a202ca7a4add23391d6226380
Mike Antekov <antekov@yandex.ru>
2020-11-30 13:00:03 +0300

    Задача 1. Рефакторинг, но WA26 на cosPhi

--- cpp/Algo/geometry/task1/solution.cpp
+++ cpp/Algo/geometry/task1/solution.cpp
@@ -45,6 +45,18 @@ public:
         return Point3D(*this);
     }
 
+    bool is_inside(double a) const {
+        return (0 <= a && a <= 1.0);
+    }
+
+    bool is_longer(double a) const {
+        return (a > 1.0);
+    }
+
+    bool is_opposite(double a) const {
+        return (a < 0);
+    }
+
     double norm() const {
         return sqrt(x * x + y * y + z * z);
     }
@@ -77,7 +89,6 @@ public:
 
     double cosPhi(const Vector3D &a) const {
         if (this->norm() * a.norm() <= DBL_EPSILON) {
-            
             return 1;
         }
         return *this * a / (this->norm() * a.norm());
@@ -99,248 +110,199 @@ Point3D::Point3D(const Vector3D& v) :
     Point3D(v.x + v.start.x, v.y + v.start.y, v.z + v.start.z) {}
 
 double Point3D::dist_to(const Vector3D &u) const {
-        //std::cerr << "u1: " << u << endl;
-        //std::cerr << "C1: " << C << endl; 
-        Vector3D w(u.start, *this);
-        //std::cerr << "w1: " << w << endl;
-        Point3D N;
-        if (u.cosPhi(w) > 0) {
-            N = u * std::min(1.0, (1/u.norm()) * w.norm() * u.cosPhi(w));
-        } else {
-            //std::cerr << "N = u.start" << endl; 
-            N = u.start;
-        }
-        //std::cerr << "M: " << M << std::endl;
-        //std::cerr << "N: " << N << std::endl;
-        return Vector3D(*this, N).norm();
-    }
-
-double dist1(Vector3D u, Point3D C) {
-    //std::cerr << "u1: " << u << endl;
-    //std::cerr << "C1: " << C << endl; 
-    Vector3D w(u.start, C);
-    //std::cerr << "w1: " << w << endl;
-    Point3D M = C, N;
+    Vector3D w(u.start, *this);
+    
+    Point3D N;
     if (u.cosPhi(w) > 0) {
         N = u * std::min(1.0, (1/u.norm()) * w.norm() * u.cosPhi(w));
     } else {
-        //std::cerr << "N = u.start" << endl; 
         N = u.start;
     }
-    //std::cerr << "M: " << M << std::endl;
-    //std::cerr << "N: " << N << std::endl;
-    return Vector3D(M, N).norm();
+    
+    return Vector3D(*this, N).norm();
 }
 
-double dist(Vector3D u, Vector3D v) {
+double dist_collinear(Vector3D u, Vector3D v) {
+    return std::min(
+            std::min(v.start.dist_to(u), v.end().dist_to(u)),
+            std::min(u.start.dist_to(v), u.end().dist_to(v))
+        );
+}
+
+double dist_coplanar(Vector3D u, Vector3D v) {
     Point3D A, B, C, D, M, N;
     A = u.start;
     B = u.end();
     C = v.start;
     D = v.end();
-
     Vector3D w(u.start, v.start);
 
+    double Det1 = -u.x * v.y + v.x * u.y;
+    double Det2 = -u.x * v.z + v.x * u.z;
+    double Det3 = -u.y * v.z + v.y * u.z;
+    double Deta = 0, Detb = 0, a, b;
+
+    if (abs(Det1) > DBL_EPSILON) {
+        Deta = -w.x * v.y + w.y * v.x;
+        Detb = u.x * w.y - u.y * w.x;
+        a = Deta / Det1;
+        b = Detb / Det1;
+    } else if (abs(Det2) > DBL_EPSILON) {
+        Deta = -w.x * v.z + w.z * v.x;
+        Detb = u.x * w.z - u.z * w.x;
+        a = Deta / Det2;
+        b = Detb / Det2;
+    } else if (abs(Det3) > DBL_EPSILON) {
+        Deta = -w.y * v.z + w.z * v.y;
+        Detb = u.y * w.z - u.z * w.y;
+        a = Deta / Det3;
+        b = Detb / Det3;
+    }
+
+    Vector3D au = u * a;
+    Vector3D bv = v * b;
+    Point3D P1, P2, P, E, O = au.end();
+
+    if (au.is_opposite(a)) {
+        M = A;
+        P1 = B;
+    } else if (au.is_inside(a)) {
+        M = au;
+        P1 = A;
+    } else {
+        //std::cerr << "Point B" << std::endl;
+        M = B;
+        P1 = A;
+    }
+
+    if (bv.is_opposite(b)) {
+        //std::cerr << "Point C" << std::endl;
+        N = C;
+        P2 = D;
+    } else if (bv.is_inside(b)) {
+        //std::cerr << "Point N" << std::endl;
+        N = bv;
+        P2 = C;
+    } else {
+        //std::cerr << "Point D" << std::endl;
+        N = D;
+        P2 = C;
+    }
+    
+    if (Vector3D(M, O).norm() < Vector3D(N, O).norm()) {
+        std::swap(M, N);
+        P = P1;
+    } else {
+        P = P2;
+    }
+    
+    Vector3D MO(M, O);
+    Vector3D PN(P, N);
+    Vector3D OP(O, P);
+    Vector3D ON(O, N);
+
+    Vector3D OE = OP * (1/OP.norm()) * MO.norm() * abs(u.cosPhi(v));
+
+    if (OE.norm() > OP.norm()) {
+        N = P;    
+    } else if (OE.norm() > ON.norm()){
+        N = Point3D(OE);
+    }
+
+    return Vector3D(M, N).norm();
+}
+
+double dist_not_coplanar(Vector3D u, Vector3D v) {
+    Point3D A, B, C, D, M, N;
+    A = u.start;
+    B = u.end();
+    C = v.start;
+    D = v.end();
+    Vector3D w(u.start, v.start);
+
+    double u2 = u * u;
+    double v2 = v * v;
+    double uv = u * v;
+    double wu = w * u;
+    double wv = w * v;
+
+    double a = (-wu * v2 + wv * uv) / (uv * uv - u2 * v2);
+    double b = (u2 * wv - wu * uv) / (uv * uv - u2 * v2);
+
+    Vector3D au = u*a;
+    Vector3D bv = v*b;
+
+    M = au.end();
+    N = bv.end();
+
+    if (au.is_inside(a) && !bv.is_inside(b)) {
+        //std::cerr << "M: Point au" << std::endl;
+        if (bv.is_opposite(b)) {
+            //std::cerr << "N: Point C" << std::endl;
+            N = C;
+        } else if (bv.is_longer(b)) {
+            //std::cerr << "N: Point D" << std::endl;
+            N = D;
+        }
+        return N.dist_to(u);        
+    } else if (!au.is_inside(a) && bv.is_inside(b)) {
+        //std::cerr << "N: Point bv" << std::endl;
+        if (au.is_opposite(a)) {
+            //std::cerr << "M: Point A" << std::endl;
+            M = A;
+        } else if (au.is_longer(a)) {
+            //std::cerr << "M: Point B" << std::endl;
+            M = B;
+        }
+        return M.dist_to(v);
+    } else if (au.is_opposite(a) && bv.is_opposite(b)) {
+        if (Vector3D(M, A).norm() < Vector3D(N, C).norm()) {
+            //std::cerr << "a < 0, b < 0, MA < NC" << endl;
+            return C.dist_to(u); 
+        } else {
+            //std::cerr << "a < 0, b < 0, MA >= NC: " << Vector3D(M, A).norm() << " >= " << Vector3D(N, C).norm() << endl;
+            return A.dist_to(v);
+        }
+    } else if (au.is_longer(a) && bv.is_longer(b)) {
+        if (Vector3D(M, B).norm() < Vector3D(N, D).norm()) {
+            //std::cerr << "a > 1, b > 1, MB < ND" << endl;
+            return D.dist_to(u); 
+        } else {
+            //std::cerr << "a > 1, b > 1, MB >= ND: " << Vector3D(M, B).norm() << " >= " << Vector3D(N, D).norm() << endl;
+            return B.dist_to(v);
+        }
+
+    } else if (au.is_opposite(a) && bv.is_longer(b)) {
+        if (Vector3D(M, A).norm() < Vector3D(N, D).norm()) {
+            //std::cerr << "a < 0, b > 1, MA < ND" << endl;
+            return D.dist_to(u); 
+        } else {
+            //std::cerr << "a < 0, b > 1, MA >= ND: " << Vector3D(M, A).norm() << " >= " << Vector3D(N, D).norm() << endl;
+            return A.dist_to(v);
+        }
+    } else if (au.is_longer(a) && bv.is_opposite(b)) {
+        if (Vector3D(M, B).norm() < Vector3D(N, C).norm()) {
+            //std::cerr << "a > 1, b < 0, MB < NC" << endl;
+            return C.dist_to(u); 
+        } else {
+            //std::cerr << "a > 1, b < 0, MB >= NC: " << Vector3D(M, B).norm() << " >= " << Vector3D(N, C).norm() << endl;
+            return B.dist_to(v);
+        }
+    } else {
+        return Vector3D(M, N).norm();
+    }
+}
+
+double dist(Vector3D u, Vector3D v) {
     if (u.isCollinear(v)) {
         //std::cerr << "AB collinear to CD" << std::endl;
-        return std::min(
-            std::min(v.start.dist_to(u), v.end().dist_to(u)),
-            std::min(u.start.dist_to(v), u.end().dist_to(v))
-        );
+        return dist_collinear(u, v);
     } else if (u.isCoplanar(v)) {
         //std::cerr << "AB coplanar to CD" << std::endl;
-        double Det1 = -u.x * v.y + v.x * u.y;
-        double Det2 = -u.x * v.z + v.x * u.z;
-        double Det3 = -u.y * v.z + v.y * u.z;
-        double Deta = 0, Detb = 0, a, b;
-
-        //std::cerr << " " << Det1 << " " << Det2 << " " << Det3 << std::endl;
-
-        if (abs(Det1) > DBL_EPSILON) {
-            Deta = -w.x * v.y + w.y * v.x;
-            Detb = u.x * w.y - u.y * w.x;
-            a = Deta / Det1;
-            b = Detb / Det1;
-        } else if (abs(Det2) > DBL_EPSILON) {
-            Deta = -w.x * v.z + w.z * v.x;
-            Detb = u.x * w.z - u.z * w.x;
-            a = Deta / Det2;
-            b = Detb / Det2;
-        } else if (abs(Det3) > DBL_EPSILON) {
-            Deta = -w.y * v.z + w.z * v.y;
-            Detb = u.y * w.z - u.z * w.y;
-            a = Deta / Det3;
-            b = Detb / Det3;
-        }
-
-        //std::cerr << "a, b = "<< a << ", " << b << endl;
-
-        Vector3D au = u * a;
-        Vector3D bv = v * b;
-
-
-        Point3D P1, P2, P, E, O = au;
-        if (a <= DBL_EPSILON) {
-            //std::cerr << "Point A" << std::endl;
-            M = A;
-            P1 = B;
-        } else if (a < 1.0) {
-            //std::cerr << "Point M" << std::endl;
-            M = au;
-            P1 = A;
-        } else {
-            //std::cerr << "Point B" << std::endl;
-            M = B;
-            P1 = A;
-        }
-
-        if (b <= DBL_EPSILON) {
-            //std::cerr << "Point C" << std::endl;
-            N = C;
-            P2 = D;
-        } else if (b < 1.0) {
-            //std::cerr << "Point N" << std::endl;
-            N = bv;
-            P2 = C;
-        } else {
-            //std::cerr << "Point D" << std::endl;
-            N = D;
-            P2 = C;
-        }
-        //std::cerr << "M: " << M << std::endl;
-        //std::cerr << "N: " << N << std::endl;
-
-        if (Vector3D(M, O).norm() < Vector3D(N, O).norm()) {
-            std::swap(M, N);
-            P = P1;
-        } else {
-            P = P2;
-        }
-
-        double cos_phi = abs(u * v / (sqrt(u * u)*sqrt(v * v)));
-        double sin_phi = sqrt(1 - cos_phi * cos_phi);
-
-        Vector3D MO(M, O);
-        Vector3D PN(P, N);
-        Vector3D OP(O, P);
-        Vector3D ON(O, N);
-
-        Vector3D OE = OP * (1/OP.norm()) * MO.norm() * cos_phi;
-
-        if (OE.norm() > OP.norm()) {
-            N = P;    
-        } else if (OE.norm() > ON.norm()){
-            N = Point3D(OE);
-        }
-
-        //std::cerr << "A: " << A << std::endl;
-        //std::cerr << "B: " << B << std::endl;
-        //std::cerr << "C: " << C << std::endl;
-        //std::cerr << "D: " << D << std::endl;
-        //std::cerr << "P: " << P << std::endl;
-        //std::cerr << "M: " << M << std::endl;
-        //std::cerr << "N: " << N << std::endl;
-        return Vector3D(M, N).norm();
-        
+        return dist_coplanar(u, v);
     } else {
-        std::cerr << "AB not coplanar to CD" << std::endl;
-
-        double u2 = u * u;
-        double v2 = v * v;
-        double uv = u * v;
-        double wu = w * u;
-        double wv = w * v;
-
-        double a = (-wu * v2 + wv * uv) / (uv * uv - u2 * v2);
-        double b = (u2 * wv - wu * uv) / (uv * uv - u2 * v2);
-
-        //std::cerr << "cos(phi) = " << uv / (sqrt(u2)*sqrt(v2)) << std::endl;
-
-        //std::cerr << "(wu) = " << (wu) << endl;
-        //std::cerr << "(wv) = " << (wv) << endl;
-        //std::cerr << "(uv - u2) = " << (uv - u2) << endl;
-        //std::cerr << "(uv - v2) = " << (uv - v2) << endl;
-        //std::cerr << "(uv*uv - u2*v2) = " << (uv*uv - u2*v2) << endl;
-
-        //std::cerr << u2 << " " << -uv << std::endl;
-        //std::cerr << uv << " " << -v2 << std::endl;
-        //std::cerr << wu << std::endl;
-        //std::cerr << wv << std::endl;
-
-            
-        
-
-        //std::cerr << "a, b = "<< a << ", " << b << endl;
-       
-
-        Vector3D au = u*a;
-        Vector3D bv = v*b;
-
-        //std::cerr << "au: " << au << endl;
-        //std::cerr << "bv: " << bv << endl;
-        M = au;
-        N = bv;
-
-        if ((0 <= a && a <= 1.0) && !(0 <=b && b <= 1.0)) {
-            //std::cerr << "M: Point au" << std::endl;
-
-            if (b <= DBL_EPSILON) {
-                //std::cerr << "N: Point C" << std::endl;
-                N = C;
-            } else if (b > 1.0) {
-                //std::cerr << "N: Point D" << std::endl;
-                N = D;
-            }
-            return dist1(u, N);
-            
-        } else if (!(0 <= a && a <= 1.0) && (0 <= b && b <= 1.0)) {
-            //std::cerr << "N: Point bv" << std::endl;
-            
-            if (a <= DBL_EPSILON) {
-                //std::cerr << "M: Point A" << std::endl;
-                M = A;
-            } else if (a > 1.0) {
-                //std::cerr << "M: Point B" << std::endl;
-                M = B;
-            }
-            return dist1(v, M);
-        } else if (a < 0 && b < 0) {
-            if (Vector3D(M, A).norm() < Vector3D(N, C).norm()) {
-                //std::cerr << "a < 0, b < 0, MA < NC" << endl;
-                return dist1(u, C); 
-            } else {
-                //std::cerr << "a < 0, b < 0, MA >= NC: " << Vector3D(M, A).norm() << " >= " << Vector3D(N, C).norm() << endl;
-                return dist1(v, A);
-            }
-        } else if (a > 1.0 && b > 1.0) {
-            if (Vector3D(M, B).norm() < Vector3D(N, D).norm()) {
-                //std::cerr << "a > 1, b > 1, MB < ND" << endl;
-                return dist1(u, D); 
-            } else {
-                //std::cerr << "a > 1, b > 1, MB >= ND: " << Vector3D(M, B).norm() << " >= " << Vector3D(N, D).norm() << endl;
-                return dist1(v, B);
-            }
-
-        } else if (a < 0 && b > 1) {
-            if (Vector3D(M, A).norm() < Vector3D(N, D).norm()) {
-                //std::cerr << "a < 0, b > 1, MA < ND" << endl;
-                return dist1(u, D); 
-            } else {
-                //std::cerr << "a < 0, b > 1, MA >= ND: " << Vector3D(M, A).norm() << " >= " << Vector3D(N, D).norm() << endl;
-                return dist1(v, A);
-            }
-        } else if (a > 1 && b < 0) {
-            if (Vector3D(M, B).norm() < Vector3D(N, C).norm()) {
-                //std::cerr << "a > 1, b < 0, MB < NC" << endl;
-                return dist1(u, C); 
-            } else {
-                //std::cerr << "a > 1, b < 0, MB >= NC: " << Vector3D(M, B).norm() << " >= " << Vector3D(N, C).norm() << endl;
-                return dist1(v, B);
-            }
-        } else {
-            return Vector3D(M, N).norm();
-        }
-      
+        //std::cerr << "AB not coplanar to CD" << std::endl;
+        return dist_not_coplanar(u, v);
     }
 }
 
